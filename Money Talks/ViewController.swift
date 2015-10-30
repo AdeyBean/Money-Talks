8b058626aa109392aea106386f15a907fa9a5afb
AdeyBean <rsaeeosk@csc.com>
2015-10-30 18:46:41 +1100

    Initial Commit

---
+++ Money Talks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Money Talks
+//
+//  Created by Ramin Saee-Oskuee on 30/10/2015.
+//  Copyright © 2015 AppOne. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
